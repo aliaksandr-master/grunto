ffcfab99ef6f6dc4a26ab3ec8b51f8f766cc7fba
Aliaksandr Pasynkau <alxe.master@gmail.com>
2014-09-14 18:13:35 +0300

    add expample of compiling config

--- grunt/check.js
+++ grunt/check.js
@@ -2,9 +2,22 @@
 
 module.exports = function (grunt, options) {
 
+	// simple run
 	this.jshint([
 		'**/*.js',
 		'!node_modules/**/*'
 	]);
+
+	// eq with previous "jshint"
+	this.grunto('jshint', function () {
+		return [
+			'**/*.js',
+			'!node_modules/**/*'
+		];
+	});
+
+	this.grunto(function () {
+		return 'done!!!';
+	});
 
 };
