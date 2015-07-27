7f6bf1bdfcebde5f7e3ef0940afe1087b2626fe2
ALiaksandr Pasynkau <alxe.master@gmail.com>
2015-07-27 23:09:36 +0300

    add tests for lib/index

--- tests/index.js
+++ tests/index.js
@@ -1,40 +1,161 @@
 'use strict';
 
-exports['base import'] = function (test) {
-	var grunt = require('grunt');
-	var init = function (grunt) {
+var _ = require('lodash');
+var grunt = require('grunt');
 
-		this.context({
-			CWD: process.cwd()
+var grunto = function (initFunc, options) {
+	require('./_lib')(initFunc, _.extend({
+		autoload: true,
+		timeMetric: false
+	}, options));
+};
+
+exports['base import'] = function (test) {
+	test.doesNotThrow(function () {
+		var gruntoFile = grunto(function (grunt) {
+			this.context({
+				CWD: process.cwd()
+			});
+
+			this.scan([
+				{
+					cwd: 'grunt/',
+					src: [
+						'**/*.js',
+						'!**/_*',
+						'!**/_*/**/*'
+					]
+				}
+			]);
+
+			return {
+				jshint: {
+					options: {
+						jshintrc: true
+					}
+				}
+			};
 		});
 
-		this.scan([
-			{
+		gruntoFile(grunt);
+	});
+
+	test.done();
+};
+
+exports['scan is plain object'] = function (test) {
+	test.doesNotThrow(function () {
+		var init = function (grunt) {
+			this.context({
+				CWD: process.cwd()
+			});
+
+			this.scan({
 				cwd: 'grunt/',
 				src: [
 					'**/*.js',
 					'!**/_*',
 					'!**/_*/**/*'
 				]
-			}
-		]);
+			});
 
-		return {
-			jshint: {
-				options: {
-					jshintrc: true
+			return {
+				jshint: {
+					options: {
+						jshintrc: true
+					}
 				}
-			}
+			};
 		};
-	};
 
-	var gruntoFile = require('./_lib')(init, {
-		autoload: true,
-		timeMetric: false
+		var gruntoFile = grunto(init);
+
+		gruntoFile(grunt);
 	});
 
-	test.ok(true);
+	test.done();
+};
+
+
+exports['scan is string'] = function (test) {
 	test.doesNotThrow(function () {
+		var gruntoFile = grunto(function (grunt) {
+
+			this.context({
+				CWD: process.cwd()
+			});
+
+			this.scan('grunt/**/*.js');
+
+			return {
+				jshint: {
+					options: {
+						jshintrc: true
+					}
+				}
+			};
+		});
+
+		gruntoFile(grunt);
+
+		test.done();
+	});
+};
+
+exports['scan is number - error'] = function (test) {
+	var gruntoFile = grunto(function (grunt) {
+		this.context({
+			CWD: process.cwd()
+		});
+
+		test.trows(function () {
+			this.scan(123);
+		});
+	});
+
+	gruntoFile(grunt);
+
+	test.done();
+};
+
+exports['context is empty'] = function (test) {
+	test.trows(function () {
+		var gruntoFile = grunto(function (grunt) {
+			this.context();
+
+			return {
+				jshint: {
+					options: {
+						jshintrc: true
+					}
+				}
+			};
+		});
+
+		gruntoFile(grunt);
+
+		test.done();
+	});
+};
+
+exports['invalid config'] = function (test) {
+	var gruntoFile = grunto(function (grunt) {
+		test.trows(function () {
+			this.config(123123);
+		});
+	});
+
+	gruntoFile(grunt);
+
+	test.done();
+};
+
+
+exports['option statistic'] = function (test) {
+	test.doesNotThrow(function () {
+		var gruntoFile = grunto(function (grunt) {
+		}, { statistic: true });
+
 		gruntoFile(grunt);
 	});
 
