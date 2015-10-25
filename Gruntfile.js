b3b3cab42225a4baa994a37dd7253341eadc02be
Aliaksandr Pasynkau <alxe.master@gmail.com>
2015-10-25 23:25:02 +0300

    big refactoring of GruntO object. add new apportunities

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,28 @@
 'use strict';
 
 module.exports = require('./lib')(function (grunt) {
+	require('jit-grunt')(grunt);
+
+	this.addTaskNames([
+		'eslint'
+	]);
+
 	this.context({
 		CWD: process.cwd()
 	});
 
-	this.scan([{
+	this.scan({
 		cwd: 'grunt/',
 		src: [
 			'**/*.js',
 			'!**/_*',
 			'!**/_*/**/*'
 		]
-	}]);
+	});
+
+	this.module('hello', function (grunt, CFG) {
+		this.$run('eslint:hello');
+	});
 
 	return {
 		eslint: {
